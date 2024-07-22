3147ecc57b761a97ee184508ec881a7b077e0536
Govamarket <govaway@gmail.com>
2024-07-22 13:46:59 +0100

    initial commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,36 +1,43 @@
-import React from 'react'
+import React from "react";
 // import { Link } from 'react-router-dom'
-import './Navbar.css'
+import { MdOutlineLegendToggle } from "react-icons/md";
+import "./Navbar.css";
 
 function Navbar() {
+function toggleNavbar () {
+  const navbarLinks = document.querySelector("nav-links");
+  navbarLinks.classList.toggle("active");
+}
+  
   return (
-
-    <div className='nav'>
-
- <div className='navbar-logos'>
-        <img className='nav-logo-1' src='./Group 1.png' />
-        <img className='nav-logo-2' src='./Group 7.png' />
+    <nav className="navbar">
+      <div className="navbar_container">
+        <div className="navbar-logos">
+          <img className="nav-logo-1" src="./Group 1.png" />
+          <img className="nav-logo-2" src="./Group 7.png" />
+  
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+      
         </div>
 
-    <div className='nav-links'>
-   <ul>
-    <li>Home</li>
-    <li>About Us</li>
-    <li>How to use</li>
-   </ul>
+        <div className="nav-links">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>How to use</li>
+            
+          </ul>
 
-   {/* nav button */}
-<section className='nav-btn'>
-<button>Log In</button>
-</section>
-    </div>
-
-    </div>
-
-    
-       
-    
-  )
+          {/* nav button */}
+          <section className="nav-btn">
+            <button>Log In</button>
+          </section>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
