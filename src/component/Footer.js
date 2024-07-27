586bc5d5e88f346e76ccb144d610f32723558900
Govamarket <govaway@gmail.com>
2024-07-27 01:56:20 +0100

    initial commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdIconName } from 'react-icons/md';
+import { MdIconName } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
 import { BsMic } from "react-icons/bs";
 import { CiImageOn } from "react-icons/ci";
@@ -69,55 +69,60 @@ function Footer() {
             <li>Blog</li>
           </ul>
         </section>
+        <section className="colum-2">
+        <div className="footer-header">
+          <p>Support</p>
+        </div>{" "}
+        <br />
+        <ul>
+          <li>Getting started</li>
+          <li>Help center</li>
+          <li>Server status</li>
+          <li>Report a bug</li>
+          <li>Chat support</li>
+        </ul>
+      </section>
 
-        <section className="colum-1">
-          <div className="footer-header">
-            <p>Support</p>
-          </div>{" "}
-          <br />
-          <ul>
-            <li>Getting started</li>
-            <li>Help center</li>
-            <li>Server status</li>
-            <li>Report a bug</li>
-            <li>Chat support</li>
-          </ul>
-        </section>
+      <section className="colum-2">
+        <div className="footer-header">
+          <p>Contact us</p>
+        </div>{" "}
+        <br />
+        <ul>
+          <li className="flec">
+            <span className="mail-con">
+              <MdOutlineEmail />
+            </span>
+            contact@company.com
+          </li>
 
-        <section className="colum-1">
-          <div className="footer-header">
-            <p>Contact us</p>
-          </div>{" "}
-          <br />
-          <ul>
-            <li className="flec">
-              <span className="mail-con">
-                <MdOutlineEmail />
-              </span>
-              contact@company.com
-            </li>
+          <li className="mob">
+            <span className="mobile">
+              <FaPhoneAlt />
+            </span>
+            (414)687-5892
+          </li>
 
-            <li className="mob">
-              <span className="mobile">
-                <FaPhoneAlt />
-              </span>
-              (414)687-5892
-            </li>
-
-            <li className="location">
-             <span className="gps-con"> <CiLocationOn /></span>
-              798 Macllister St San Francisco, 94102
-            </li>
-            <li>Reviews</li>
-            <li>Updates</li>
-          </ul>
-        </section>
+          <li className="location">
+            <span className="gps-con">
+              {" "}
+              <CiLocationOn />
+            </span>
+            798 Macllister St San Francisco, 94102
+          </li>
+          <li>Reviews</li>
+          <li>Updates</li>
+        </ul>
+      </section>
       </div>
 
+      
       <section className="all-right">
-    <p>© Copyright 2011-2021 www.dapplab.co All rights reserved. Developed by Wecode.</p>
+        <p>
+          © Copyright 2011-2021 www.dapplab.co All rights reserved. Developed by
+          Wecode.
+        </p>
       </section>
-      <hr></hr>
     </div>
   );
 }
