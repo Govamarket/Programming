a976d9e5cfbaf7e085ba9d883e50f4723f906fcb
Govamarket <govaway@gmail.com>
2024-07-29 15:19:06 +0100

    initial commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,38 +4,62 @@ import { MdOutlineLegendToggle } from "react-icons/md";
 import "./Navbar.css";
 
 function Navbar() {
-function toggleNavbar () {
-  const navbarLinks = document.querySelector("nav-links");
-  navbarLinks.classList.toggle("active");
-}
-  
+  function toggleNavbar() {
+    const navSlide = () => {
+      const burger = document.querySelector(".burger-handle");
+      const nav = document.querySelector(".nav-links");
+      const navLinks = document.querySelectorAll(".nav-links ul");
+    
+      // the toggle navigation
+      burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+        // Animate links
+        navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+            link.style.animation = "";
+          } else {
+            link.style.animation = `navLinksFade 0.5s ease forwards ${
+              index / 7 + 1.5
+            }s`;
+          }
+        });
+        // Burger animation
+        burger.classList.toggle("toggle");
+      });
+    };
+    
+    navSlide();
+  }
+
   return (
-    <nav className="navbar">
-      <div className="navbar_container">
+    <nav>
+      <div className="nav-links">
         <div className="navbar-logos">
           <img className="nav-logo-1" src="./Group 1.png" />
           <img className="nav-logo-2" src="./Group 7.png" />
-  
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-      
         </div>
-
-        <div className="nav-links">
-          <ul>
-            <li>Home</li>
+        
+        <ul>
+        <li>Home</li>
             <li>About Us</li>
             <li>How to use</li>
-            
-          </ul>
+        </ul>
+           
+          
 
           {/* nav button */}
           <section className="nav-btn">
             <button>Log In</button>
           </section>
-        </div>
+
+        <div className="burger-handle">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
       </div>
+
+     
     </nav>
   );
 }
